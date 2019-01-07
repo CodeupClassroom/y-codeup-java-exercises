1b63dac76124e20b0326f758a955252de12d4257
fer <fernandoza@gmail.com>
2019-01-07 12:33:26 -0600

    feat: first part of the lecture

---
+++ src/Basics.java
@@ -0,0 +1,25 @@
+public class Basics {
+
+    public static void main(String[] args) {
+        String name = "Fernando \\ Fer";
+        byte dayOfWeek = 7;
+        char gender = 'F';
+        int numOfStudents = 17;
+        long max = 2342343242344L;
+        double price = 2.27;
+        boolean yesOrNo = false;
+
+
+        long const;
+
+        System.out.println(name);
+
+        System.out.println(numOfStudents);
+
+        System.out.println(price);
+
+        System.out.println(max);
+
+    }
+
+}
