7abd168098fb91d677918427655a61524d065f26
fer <fernandoza@gmail.com>
2019-01-07 15:49:22 -0600

    feat: finish the exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,41 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Fer");
-        System.out.print("Mendoza");
+        int myFavoriteNumber = 1;
+        String myString = "Fer";
+        float myNumber = 3.14f;
+
+        System.out.println(myNumber);
+        System.out.println(myString);
+        System.out.println("Mendoza");
+
+//        int x = 5;
+//        System.out.println(++x);
+
+        String theNumberThree = "3";
+        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println(three);
+
+        int three = Integer.parseInt("3");
+        System.out.println(three);
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
+        int newNumber = Integer.MAX_VALUE +1;
+        System.out.println(newNumber);
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
     }
 }
