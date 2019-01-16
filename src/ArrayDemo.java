048a58a849b23c41af9246ece3e7375f96307a5c
RyanOrsinger <ryan.orsinger@gmail.com>
2019-01-16 17:02:43 -0600

    Add array example

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -20,6 +20,7 @@ public class ArrayDemo {
 
         String[] names = new String[numberOfNames];
 
+
         int i = 0;
         do {
             names[i] = input.getString("What is the name?");
@@ -31,5 +32,28 @@ public class ArrayDemo {
             System.out.println("-----");
             System.out.println(name);
         }
+
+        // behavior that looks like growing an array
+        System.out.println(numbers);
+
+        numbers = addNumberToArray(numbers, 44);
+        System.out.println(numbers);
+
+        for(int num : numbers) {
+            System.out.println(num);
+        }
+    }
+
+    public static int[] addNumberToArray(int[] array, int newNumber) {
+        int[] newArray = new int[array.length + 1];
+
+        for(int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+
+        newArray[array.length] = newNumber;
+
+        return newArray;
+
     }
 }
